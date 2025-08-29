d44211a3b907f720a67eb0b2ff962d8ee88581f7
ogrin <olgagrin2011@gmail.com>
2025-08-29 16:09:37 -0400

    Added lombok

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -13,6 +13,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/users")
+@CrossOrigin(origins = "*")
 public class UserController {
 
     @Autowired
--- src/main/java/com/example/entity/User.java
+++ src/main/java/com/example/entity/User.java
@@ -5,7 +5,14 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.*;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
 @Entity
 @Table(name = "users")
 public class User {
@@ -26,46 +33,8 @@ public class User {
     @Size(min = 5, max = 254, message = "Email must be between 5 and 254 characters")
     private String email;
 
-    // Default constructor
-    public User() {}
-
-    // Constructor with parameters
     public User(String name, String email) {
         this.name = name;
         this.email = email;
-    }
-
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                '}';
     }
 }
